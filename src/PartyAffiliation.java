3fe88aba4bf5b73f937f3f5b8b9b06c02471f963
Lakshmi Charan Grandhisiri <grandhln@mail.uc.edu>
2026-02-15 15:17:07 -0500

    PA Done

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class PartyAffiliation {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("Party Affiliation Menu:");
+        System.out.println("D - Democrat");
+        System.out.println("R - Republican");
+        System.out.println("I - Independent");
+        System.out.print("Enter your choice: ");
 
+        String choice = in.nextLine();
+
+        if (choice.equalsIgnoreCase("D")) {
+            System.out.println("You get a Democratic Donkey.");
+        } else if (choice.equalsIgnoreCase("R")) {
+            System.out.println("You get a Republican Elephant.");
+        } else if (choice.equalsIgnoreCase("I")) {
+            System.out.println("You get an Independent Person.");
+        } else {
+            System.out.println("You get an Other.");
+        }
+
+        in.close();
     }
 }
